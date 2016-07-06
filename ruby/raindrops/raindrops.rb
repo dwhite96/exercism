67b25bbe371666ea3847835917f3c00ee0dcb55c
David White <whited75@gmail.com>
2016-07-06 12:57:06 -0600

    Raindrops first iteration

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,16 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Raindrops
+  def self.convert(num)
+    string = ""
+
+    string << "Pling" if num % 3 == 0
+    string << "Plang" if num % 5 == 0
+    string << "Plong" if num % 7 == 0
+    string = num.to_s if string.empty?
+
+    string
+  end
+end
