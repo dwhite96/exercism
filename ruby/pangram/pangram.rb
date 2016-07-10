98c0cc5290707fb9d920ba7fd916d816d8ba39bd
David White <whited75@gmail.com>
2016-07-10 14:59:00 -0600

    Second iteration of pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -3,10 +3,8 @@ module BookKeeping
 end
 
 class Pangram
+  ALPHABET = ('a'..'z').to_a
   def self.is_pangram?(str)
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    count = 0
-    alphabet.each_char { |char| count += 1 if str.downcase.include? char }
-    count >= 26
+    ALPHABET & str.downcase.chars == ALPHABET
   end
 end
